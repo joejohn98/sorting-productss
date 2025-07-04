b9333700e96b9af82ce43b3fe7e22c69d452f131
joejohn98 <joejohn048@gmail.com>
2025-07-04 23:49:48 +0530

    Add Product interface and initialize products state in ProductsList component

--- src/ProductsList.tsx
+++ src/ProductsList.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react"
+import type { Product } from "./types"
 
 const ProductsList = () => {
+    const [products, setProducts] = useState<Product[]>([])
   return (
     <div>
       
